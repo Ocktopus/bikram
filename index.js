9d7ebb9534cb3bd1c06d1faa4a1d2a15d12ad481
ocktopus <pxocktopus@gmail.com>
2023-06-16 14:27:42 +0200

    guardado antes de comer

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1.- Crear variable tipo **let** de nombre **variableSinValor** declarada **sin valor**
+
+let variableSinValor;
+
+//2.- Crear **2 variables** tipo **let** de nombres **booleano1** y **booleano2** con valores **booleanos**
+
+let booleano1=true;
+let booleano2=false;
+
+//3.- Crear variable tipo **const** de nombre **PI** declarada con valor **3.14**
+
+const PI=3.14;
+
+//4.- Crear variable tipo **const** de nombre **TAU** declarada con valor **2 veces PI**
+
+const TAU=2*PI;
+
+//5.- Crear variable **booleanoAnd** cuyo valor sea la comparación booleana **booleano1 and booleano2**
+
+let booleanoAnd=booleano1&&booleano2;
+
+//6.- Crear variable **booleanoNot** cuyo valor sea la compracación booleana **no booleano1**
+
+let booleanoNot=booleano2;
+
+//7.- Crear variable **booleanoMix0** cuyo valor sea la compración booleana **(booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)**
+
+let booleanoMix0= (booleano1 || booleano2 ) && (booleano1 ||(!booleano1 && !booleano2))
+
+//8.- Crear variable **incrementarDesp** con valor **2** y **asigna su valor con postincremento a resultadoDesp**
+
+let incrementarDesp = 2;
+let resultadoDesp= incrementarDesp+=;
